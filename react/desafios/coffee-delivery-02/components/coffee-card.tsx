8e2930d92d8d1e64156bc44d6496a27bffbea607
Bernardo Nunes <bmn02@outlook.com>
2023-11-14 00:18:50 -0300

    fix: coffee-card to serve component

--- react/desafios/coffee-delivery-02/components/coffee-card.tsx
+++ react/desafios/coffee-delivery-02/components/coffee-card.tsx
@@ -1,22 +1,15 @@
-'use client'
-
 import React from 'react'
 import { Coffee } from '@/data/types/coffee'
 import Image from 'next/image'
 import { CountButton } from './count-button'
 import { CoffeeSubmitButton } from './coffee-submit-button'
 import { CoffeeFormProvider } from '@/context/coffee-form-context'
-import { useStore } from '@/store'
 
 interface CoffeeProps {
   coffee: Coffee
 }
 
 export const CoffeeCard = ({ coffee }: CoffeeProps) => {
-  const { wishCoffees } = useStore()
-
-  console.log(wishCoffees)
-
   return (
     <div
       key={coffee.id}
